10a1a986351bbb797c878e0e583845d852a2dcbc
Musawer Khan Fatih <63827449+musawerfatih@users.noreply.github.com>
2023-03-31 14:27:55 +0500

    Add files via upload

---
+++ data.py
@@ -0,0 +1,261 @@
+# import requests
+
+# parameters = {
+#     "amount":10,
+#     "type":"boolean",
+# }
+
+# response = requests.get("https://opentdb.com/api.php", params=parameters)
+# response.raise_for_status()
+
+# data = response.json()
+# question_data = data["results"]
+
+
+# --------------------------------- GENERAL QATEGORY QUESTIONS --------------------------------- #
+
+# question_data = [
+#     {
+#         "category": "Science: Computers",
+#         "type": "boolean",
+#         "difficulty": "medium",
+#         "question": "The HTML5 standard was published in 2014.",
+#         "correct_answer": "True",
+#         "incorrect_answers": [
+#             "False"
+#         ]
+#     },
+#     {
+#         "category": "Science: Computers",
+#         "type": "boolean",
+#         "difficulty": "medium",
+#         "question": "The first computer bug was formed by faulty wires.",
+#         "correct_answer": "False",
+#         "incorrect_answers": [
+#             "True"
+#         ]
+#     },
+#     {
+#         "category": "Science: Computers",
+#         "type": "boolean",
+#         "difficulty": "medium",
+#         "question": "FLAC stands for 'Free Lossless Audio Condenser'.",
+#         "correct_answer": "False",
+#         "incorrect_answers": [
+#             "True"
+#         ]
+#     },
+#     {
+#         "category": "Science: Computers",
+#         "type": "boolean",
+#         "difficulty": "medium",
+#         "question": "All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.",
+#         "correct_answer": "False",
+#         "incorrect_answers": [
+#             "True"
+#         ]
+#     },
+#     {
+#         "category": "Science: Computers",
+#         "type": "boolean",
+#         "difficulty": "easy",
+#         "question": "Linus Torvalds created Linux and Git.",
+#         "correct_answer": "True",
+#         "incorrect_answers": [
+#             "False"
+#         ]
+#     },
+#     {
+#         "category": "Science: Computers",
+#         "type": "boolean",
+#         "difficulty": "easy",
+#         "question": "The programming language 'Python' is based off a modified version of 'JavaScript'",
+#         "correct_answer": "False",
+#         "incorrect_answers": [
+#             "True"
+#         ]
+#     },
+#     {
+#         "category": "Science: Computers",
+#         "type": "boolean",
+#         "difficulty": "medium",
+#         "question": "AMD created the first consumer 64-bit processor.",
+#         "correct_answer": "True",
+#         "incorrect_answers": [
+#             "False"
+#         ]
+#     },
+#     {
+#         "category": "Science: Computers",
+#         "type": "boolean",
+#         "difficulty": "easy",
+#         "question": "'HTML' stands for Hypertext Markup Language.",
+#         "correct_answer": "True",
+#         "incorrect_answers": [
+#             "False"
+#         ]
+#     },
+#     {
+#         "category": "Science: Computers",
+#         "type": "boolean",
+#         "difficulty": "easy",
+#         "question": "In most programming languages, the operator ++ is equivalent to the statement '+= 1'.",
+#         "correct_answer": "True",
+#         "incorrect_answers": [
+#             "False"
+#         ]
+#     },
+#     {
+#         "category": "Science: Computers",
+#         "type": "boolean",
+#         "difficulty": "hard",
+#         "question": "The IBM PC used an Intel 8008 microprocessor clocked at 4.77 MHz and 8 kilobytes of memory.",
+#         "correct_answer": "False",
+#         "incorrect_answers": [
+#             "True"
+#         ]
+#     }
+# ]
+
+
+# --------------------------------- COMPUTER SCIENCE QUESTIONS --------------------------------- #
+
+# question_data = [{
+#     "category": "Science: Computers",
+#     "type": "boolean",
+#     "difficulty": "medium",
+#     "question":
+#     "The common software-programming acronym &quot;I18N&quot; comes from the term &quot;Interlocalization&quot;.",
+#     "correct_answer": "False",
+#     "incorrect_answers": ["True"]
+# }, {
+#     "category": "Science: Computers",
+#     "type": "boolean",
+#     "difficulty": "medium",
+#     "question": "The first computer bug was formed by faulty wires.",
+#     "correct_answer": "False",
+#     "incorrect_answers": ["True"]
+# }, {
+#     "category": "Science: Computers",
+#     "type": "boolean",
+#     "difficulty": "easy",
+#     "question": "RAM stands for Random Access Memory.",
+#     "correct_answer": "True",
+#     "incorrect_answers": ["False"]
+# }, {
+#     "category": "Science: Computers",
+#     "type": "boolean",
+#     "difficulty": "medium",
+#     "question":
+#     "The open source program Redis is a relational database server.",
+#     "correct_answer": "False",
+#     "incorrect_answers": ["True"]
+# }, {
+#     "category": "Science: Computers",
+#     "type": "boolean",
+#     "difficulty": "hard",
+#     "question": "DHCP stands for Dynamic Host Configuration Port.",
+#     "correct_answer": "False",
+#     "incorrect_answers": ["True"]
+# }, {
+#     "category": "Science: Computers",
+#     "type": "boolean",
+#     "difficulty": "easy",
+#     "question":
+#     "The Windows ME operating system was released in the year 2000.",
+#     "correct_answer": "True",
+#     "incorrect_answers": ["False"]
+# }, {
+#     "category": "Science: Computers",
+#     "type": "boolean",
+#     "difficulty": "easy",
+#     "question":
+#     "The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen resolution.",
+#     "correct_answer": "False",
+#     "incorrect_answers": ["True"]
+# }, {
+#     "category": "Science: Computers",
+#     "type": "boolean",
+#     "difficulty": "easy",
+#     "question": "Linux was first created as an alternative to Windows XP.",
+#     "correct_answer": "False",
+#     "incorrect_answers": ["True"]
+# }, {
+#     "category": "Science: Computers",
+#     "type": "boolean",
+#     "difficulty": "medium",
+#     "question":
+#     "A Boolean value of &quot;0&quot; represents which of these words?",
+#     "correct_answer": "False",
+#     "incorrect_answers": ["True"]
+# }, {
+#     "category": "Science: Computers",
+#     "type": "boolean",
+#     "difficulty": "hard",
+#     "question":
+#     "The T-Mobile Sidekick smartphone is a re-branded version of the Danger Hiptop.",
+#     "correct_answer": "True",
+#     "incorrect_answers": ["False"]
+# }]
+
+
+# --------------------------------- ENGLISH QUESTIONS --------------------------------- #
+
+question_data = [{
+    'question':
+    'The subject of a sentence can be a noun or pronoun but never a gerund or infinitive.',
+    'correct_answer': 'False'
+}, {
+    'question': 'Transitive verbs are always followed by a direct object.',
+    'correct_answer': 'True'
+}, {
+    'question':
+    'A clause is a group of words containing a subject and verb and used as part of a sentence.',
+    'correct_answer': 'True'
+}, {
+    'question':
+    'Sentences that use the subject-verb-object order are passive voice sentences.',
+    'correct_answer': 'False'
+}, 
+{
+    'question':
+    'Either "who" or "whom" may appear as the first word in a question.',
+    'correct_answer': 'True'
+}, {
+    'question':
+    'Journalists should use personification to make their writing more interesting.',
+    'correct_answer': 'False'
+}, {
+    'question':
+    'Correct spelling is as important for writers in broadcast journalism as it is for those in print journalism.',
+    'correct_answer': 'True'
+}, {
+    'question': 'Newswriters avoid adverbs and adjectives.',
+    'correct_answer': 'True'
+}, {
+    'question': 'Longer verbs are usually stronger than shorter verbs.',
+    'correct_answer': 'False'
+},
+{
+    'question':
+    'Some words that started out as slang have won acceptance as standard English.',
+    'correct_answer': 'True'
+}]
+# {
+#     'question':
+#     'Journalists should use clichÃ©s to amuse or interest the reader.',
+#     'correct_answer': 'False'
+# }, {
+#     'question':
+#     'Except in extraordinary circumstances, journalists should remain neutral observers. They should not mention themselves in stories.',
+#     'correct_answer': 'True'
+# }, {
+#     'question':
+#     'Journalists avoid using exclamation points, dashes and parentheses.',
+#     'correct_answer': 'True'
+# }, {
+#     'question':
+#     'Skilled reporters can transform even routine events into front-page stories.',
+#     'correct_answer': 'True'
+# }
+# ]
---
+++ ui.py
@@ -0,0 +1,71 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
+THEME_COLOR = "#375362"
+
+class QuizInterface():
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title("Quizzier")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        # LABELS
+        self.score_label = Label(text="Score: 0", bg=THEME_COLOR, fg="white", font=("Arial", 12, 'normal'))
+        self.score_label.grid(column=1, row=0)
+        self.check_label = Label(text="", bg=THEME_COLOR, font=("Arial", 15, 'bold'))
+        self.check_label.grid(column=0, row=2, columnspan=2)
+
+        # CANVAS SET-UP
+        self.canvas = Canvas(width=300, height=250,bg="white")
+        self.question_text = self.canvas.create_text(150, 125, width=300, text="Amazon acquared Twitch in August 2014 for $970 million dollars.", font=("Arial", 15, "italic"))
+        self.final_score = self.canvas.create_text(150, 195,text="", font=("Arial", 12, "bold"))
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=30)
+
+        # BUTTONS
+        self.true_image = PhotoImage(file="34-Day-quizzler-app-start/images/true.png")
+        self.false_image = PhotoImage(file="34-Day-quizzler-app-start/images/false.png")
+        self.true_button = Button(image=self.true_image, highlightthickness=0, command=self.true_pressed)
+        self.false_button = Button(image=self.false_image, highlightthickness=0, command=self.false_pressed)
+        self.true_button.grid(column=1, row=3, pady=30)
+        self.false_button.grid(column=0, row=3)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        self.check_label.config(text="")
+
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz.")
+            self.canvas.itemconfig(self.final_score, text=f"Final score: {self.quiz.score}/{self.quiz.question_number}")
+            self.true_button.config(state='disabled')
+            self.false_button.config(state='disabled')
+
+
+    def true_pressed(self):
+        self.is_right = self.quiz.check_answer("True")
+        self.give_feedback(self.is_right)
+
+
+    def false_pressed(self):
+        self.is_right = self.quiz.check_answer("False")
+        self.give_feedback(self.is_right)
+
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+            self.check_label.config(text="Correct!", fg="#9af843")
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+        else:
+            self.canvas.config(bg="red")
+            self.check_label.config(text="Incorrect!", fg="red")
+
+        self.timer = self.window.after(1000, func=self.get_next_question)
